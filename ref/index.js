7bcac17f8860eec14af275dbd0cde67499b6cdfc
Mosball <rnjsrldnd123@naver.com>
2019-08-28 23:41:37 +0900

    회원가입 폼 완성 / input 포커스시 border색 변경

---
+++ ref/index.js
@@ -0,0 +1,19 @@
+const registerInputFocusEvent = () => {
+    const inputs = document.querySelectorAll('.join-form-box-input > *')
+
+    inputs.forEach(input => {
+        input.addEventListener('focus', (e) => {
+            e.target.style.border = "1px solid #5a96ff"
+        })
+
+        input.addEventListener('focusout', (e) => {
+            e.target.style.border = "1px solid #e0e0e0"
+        })
+    })
+    
+}
+
+window.onload = () => {
+    registerInputFocusEvent()
+}
+
