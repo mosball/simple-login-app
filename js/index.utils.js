904ec38ef4f48d231566bb7f7b03466c1553f160
Mosball <rnjsrldnd123@naver.com>
2019-08-29 14:39:59 +0900

    css/js 폴더 생성 및 index.utils.js파일을 생성해서 기타 함수들을 utils 오브젝트로 묶음

---
+++ js/index.utils.js
@@ -0,0 +1,35 @@
+const utils = {
+    registerAllUtilsEvents() {
+        this.registerInputFocusEvent()
+        this.registerShowBtnClickEvent()
+    },
+    registerInputFocusEvent() {
+        const inputs = document.querySelectorAll('.join-form-box-input > *')
+    
+        inputs.forEach(input => {
+            input.addEventListener('focus', (e) => {
+                e.target.style.border = "1px solid #5a96ff"
+            })
+    
+            input.addEventListener('focusout', (e) => {
+                e.target.style.border = "1px solid #e0e0e0"
+            })
+        })
+    },
+    registerShowBtnClickEvent() {
+        const showLogin = document.querySelector('#show-login')
+        const showJoin  = document.querySelector('#show-join')
+    
+        showLogin.addEventListener('click', (e) => {
+            document.querySelector('#default-body').style.display = "none"
+            document.querySelector('#join-form').style.display = "none"
+            document.querySelector('#login-form').style.display = "block"
+        })
+    
+        showJoin.addEventListener('click', (e) => {
+            document.querySelector('#default-body').style.display = "none"
+            document.querySelector('#login-form').style.display = "none"
+            document.querySelector('#join-form').style.display = "block"
+        })
+    }
+}
