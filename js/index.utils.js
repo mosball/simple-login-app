52404907c0f0f6af6dd4f11aba3d2dcc0f5f386e
Mosball <rnjsrldnd123@naver.com>
2019-08-30 18:42:54 +0900

    index.validation.js 가독성 개선을 위한 리펙토링 진행

--- js/index.utils.js
+++ js/index.utils.js
@@ -1,21 +1,6 @@
 const utils = {
     registerAllUtilsEvents() {
-        this.registerInputFocusEvent()
         this.registerShowBtnClickEvent()
-    },
-
-    registerInputFocusEvent() {
-        const inputs = document.querySelectorAll('.join-form-box-input > *')
-    
-        inputs.forEach(input => {
-            input.addEventListener('focus', (e) => {
-                e.target.style.border = "1px solid #5a96ff"
-            })
-    
-            input.addEventListener('focusout', (e) => {
-                e.target.style.border = "1px solid #e0e0e0"
-            })
-        })
     },
     
     registerShowBtnClickEvent() {
--- js/index.validation.js
+++ js/index.validation.js
@@ -1,35 +1,218 @@
 const validationChecker = {
     registerAllCheckEvent() {
-        this.registerIdCheckEvent()
-        this.registerPasswordCheckEvent()
-        this.registerPassword2CheckEvent()
-        this.registerBirthYearCheckEvent()
-        this.registerMonthDayCheckEvent()
-        this.registerBirthDayCheckEvent()
-        this.registerEmailCheckEvent()
-        this.registerPhoneCheck()
+        this.registerCheckEvent('#id-field input[name=id]')
+        this.registerCheckEvent('#password-field input[name=password]')
+        this.registerCheckEvent('#password2-field input[name=password2]')
+        this.registerCheckEvent('#birth-field input[name=year]')
+        this.registerCheckEvent('#birth-field select[name=month]')
+        this.registerCheckEvent('#birth-field input[name=day]')
+        this.registerCheckEvent('#email-field input[name=email]')
+        this.registerCheckEvent('#phone-field input[name=phone]')
     },
 
-    checkInputLength(input, start, end) {
-        try {
-            return start <= input.length && input.length <= end
-        } catch (e) {
-            return false
+    insertInfoMsg(element, msg, css) {
+        const infoMsgTarget    = element.getAttribute('data-target')
+        const infoMsgField     = document.querySelector(infoMsgTarget)
+
+        infoMsgField.innerHTML = `<span class = "${css}">${msg}</span>`
+    },
+
+    getFieldText(field) {
+        return field.tagName === 'INPUT' ?
+        field.value :
+        field.options[field.selectedIndex].value
+    },
+
+    getCheckFunction(name) {
+        switch (name) {
+            case 'id'       : return this.checkId
+            case 'password' : return this.checkPassword
+            case 'password2': return this.checkPassword2
+            case 'year'     : return this.checkBirthYear
+            case 'month'    : return this.checkBirthMonth
+            case 'day'      : return this.checkBirthDay.bind(this)
+            case 'email'    : return this.checkEmail
+            case 'phone'    : return this.checkPhone
         }
     },
 
-    checkInputText(input, regExp) {
-        return regExp.test(input)
+    registerCheckEvent(cssSelector) {
+        const someInputField = document.querySelector(cssSelector)
+
+        someInputField.addEventListener('focusout', (e) => {
+            const text  = this.getFieldText(e.target)
+            const check = this.getCheckFunction(e.target.getAttribute('name'))
+
+            check(text, (result) => {
+                if (!result) return
+                this.insertInfoMsg(e.target, result.msg, result.css)
+            })
+        })
     },
 
-    checkOverlapId(id) {
-        return true
+    checkId(id, callback) {
+        const checkList = [
+            {
+                condition: id === '',
+                msg      : '',
+                css      : ''
+            },
+            {
+                condition: !/^[a-z0-9\-\_]{5,20}$/.test(id),
+                msg      : '5~20자의 영문 소문자, 숫자와 특수기호(_)(-)만 사용 가능합니다.',
+                css      : 'red-text'
+            },
+            {
+                condition: false, 
+                msg      : '이미 사용중인 아이디입니다.',
+                css      : 'red-text'
+            },
+            {
+                condition: true,
+                msg      : '사용 가능한 아이디입니다.',
+                css      : 'green-text'
+            },
+        ]
+
+        //아이디 중복 검사
+
+        callback(checkList.find(e => e.condition === true))
     },
 
-    insertInfoMsg(element, msg, color) {
-        const infoMsgTarget = element.getAttribute('data-target')
-        const infoMsgField  = document.querySelector(infoMsgTarget)
-        infoMsgField.innerHTML = `<span class = "${color}-text">${msg}</span>`
+    checkPassword(password, callback) {
+        const checkList = [
+            {
+                condition: password === '',
+                msg      : '',
+                css      : ''
+            },
+            {
+                condition: password.length < 8 || 16 < password.length,
+                msg      : '8자 이상 16자 이하로 입력하세요.',
+                css      : 'red-text'
+            },
+            {
+                condition: !/[A-Z]/.test(password), 
+                msg      : '영문 대문자를 최소 1자 이상 포함하세요.',
+                css      : 'red-text'
+            },
+            {
+                condition: !/[0-9]/.test(password), 
+                msg      : '숫자를 최소 1자 이상 포함하세요.',
+                css      : 'red-text'
+            },
+            {
+                condition: !/[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/.test(password), 
+                msg      : '특수문자를 최소 1자 이상 포함하세요.',
+                css      : 'red-text'
+            },
+            {
+                condition: true,
+                msg      : '안전한 비밀번호입니다.',
+                css      : 'green-text'
+            },
+        ]
+
+        callback(checkList.find(e => e.condition === true))
+    },
+
+    checkPassword2(password2, callback) {
+        const checkList = [
+            {
+                condition: password2 === '',
+                msg      : '',
+                css      : ''
+            },
+            {
+                condition: document.querySelector('#password-field input[name=password]').value !== password2,
+                msg      : '비밀번호가 일치하지 않습니다.',
+                css      : 'red-text'
+            },
+            {
+                condition: true,
+                msg      : '비밀번호가 일치합니다.',
+                css      : 'green-text'
+            },
+        ]
+
+        callback(checkList.find(e => e.condition === true))
+    },
+
+    checkBirthYear(year, callback) {
+        year = year !== '' ? Number(year) : year
+
+        const currentYear = new Date().getFullYear()
+        const lessThan99  = currentYear - 98
+        const moreThan15  = currentYear - 14
+
+        const checkList = [
+            {
+                condition: year === '',
+                msg      : '',
+                css      : ''
+            },
+            {
+                condition: isNaN(year),
+                msg      : '태어난 년도 4자리를 정확하게 입력하세요.',
+                css      : 'red-text'
+            },
+            {
+                condition: lessThan99 > year,
+                msg      : '100세 이하만 가입 가능합니다.',
+                css      : 'red-text'
+            },
+            {
+                condition: moreThan15 < year,
+                msg      : '만 14세 이상만 가입 가능합니다.',
+                css      : 'red-text'
+            },
+            {
+                condition: true,
+                msg      : '',
+                css      : ''
+            },
+        ]
+
+        callback(checkList.find(e => e.condition === true))
+    },
+
+    checkBirthMonth(text, callback) {
+        if (text !== '월') {
+            document.querySelector('#birth-field input[name=day]').focus()
+        }
+
+        callback(null)
+    },
+
+    checkBirthDay(day, callback) {
+        day = Number(day)
+
+        const monthText = this.getFieldText(document.querySelector('#birth-field select[name=month]'))
+        const lastDay   = this.getLastDay(monthText)
+        const checkList = [
+            {
+                condition: day === '',
+                msg      : '',
+                css      : ''
+            },
+            {
+                condition: monthText === '월',
+                msg      : '태어난 월을 먼저 선택하세요.',
+                css      : 'red-text'
+            },
+            {
+                condition: isNaN(day) || day < 0 || lastDay < day,
+                msg      : '태어난 날짜를 다시 확인하세요.',
+                css      : 'red-text'
+            },
+            {
+                condition: true,
+                msg      : '',
+                css      : ''
+            },
+        ]
+
+        callback(checkList.find(e => e.condition === true))
     },
 
     getLastDay(month) {
@@ -40,159 +223,49 @@ const validationChecker = {
         return date.getDate()
     },
 
-    registerIdCheckEvent() {
-        const idInputField = document.querySelector('#id-field input[name=id]')
+    checkEmail(email, callback) {
+        email = email.toLowerCase()
 
-        idInputField.addEventListener('focusout', (e) => {
-            const text = e.target.value
-            if (text === '') return
+        const checkList = [
+            {
+                condition: email === '',
+                msg      : '',
+                css      : ''
+            },
+            {
+                condition: !/^[a-z]+[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/.test(email),
+                msg      : '이메일 주소를 다시 확인해주세요.',
+                css      : 'red-text'
+            },
+            {
+                condition: true,
+                msg      : '',
+                css      : ''
+            },
+        ]
 
-            if (!this.checkInputLength(text, 5, 20) || !this.checkInputText(text, /^[a-z0-9\-\_]+$/)) {
-                this.insertInfoMsg(e.target, 
-                    '5~20자의 영문 소문자, 숫자와 특수기호(_)(-) 만 사용 가능합니다.', 
-                    'red')
-
-            } else if (!this.checkOverlapId(text)) {
-                this.insertInfoMsg(e.target, '이미 사용중인 아이디입니다.', 'red')
-                
-            } else {
-                this.insertInfoMsg(e.target, '사용 가능한 아이디입니다.', 'green')
-            }
-        })
+        callback(checkList.find(e => e.condition === true))
     },
 
-    registerPasswordCheckEvent() {
-        const passwordInputField = document.querySelector('#password-field input[name=password]')
+    checkPhone(phone, callback) {
+        const checkList = [
+            {
+                condition: phone === '',
+                msg      : '',
+                css      : ''
+            },
+            {
+                condition: !/^(010)[0-9]{7,8}$/.test(phone),
+                msg      : '형식에 맞지 않는 번호입니다.',
+                css      : 'red-text'
+            },
+            {
+                condition: true,
+                msg      : '',
+                css      : ''
+            },
+        ]
 
-        passwordInputField.addEventListener('focusout', (e) => {
-            const text = e.target.value
-            if (text === '') return
-
-            if (!this.checkInputLength(text, 8, 16)) {
-                this.insertInfoMsg(e.target, '8자 이상 16자 이하로 입력하세요.', 'red')
-                
-            } else if (!this.checkInputText(text, /[A-Z]/)) {
-                this.insertInfoMsg(e.target, '영문 대문자를 최소 1자 이상 포함하세요.', 'red')
-                
-            } else if (!this.checkInputText(text, /[0-9]/)) {
-                this.insertInfoMsg(e.target, '숫자를 최소 1자 이상 포함하세요.', 'red')
-                
-            } else if (!this.checkInputText(text, /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/)) {
-                this.insertInfoMsg(e.target, '특수문자를 최소 1자 이상 포함하세요.', 'red')
-                
-            } else {
-                this.insertInfoMsg(e.target, '안전한 비밀번호입니다.', 'green')
-            }
-        })
-    },
-
-    registerPassword2CheckEvent() {
-        const passwordInputField  = document.querySelector('#password-field input[name=password]')
-        const password2InputField = document.querySelector('#password2-field input[name=password2]')
-
-        password2InputField.addEventListener('input', (e) => {
-            const text  = e.target.value
-            const text2 = passwordInputField.value
-            if (text === '') return
-
-            if (text === text2) {
-                this.insertInfoMsg(e.target, '비밀번호가 일치합니다.', 'green')
-            } else {
-                this.insertInfoMsg(e.target, '비밀번호가 일치하지 않습니다.', 'red')
-            }
-        })
-    },
-
-    registerBirthYearCheckEvent() {
-        const birthYearInputField = document.querySelector('#birth-field input[name=year]')
-
-        birthYearInputField.addEventListener('focusout', (e) => {
-            let year = e.target.value
-            if (year === '') return
-            
-            const currentYear = new Date().getFullYear()
-            const lessThan99  = currentYear - 98
-            const moreThan15  = currentYear - 14
-            year = Number(year)
-
-            if (isNaN(year)) {
-                this.insertInfoMsg(e.target, '태어난 년도 4자리를 정확하게 입력하세요.', 'red')
-
-            } else if (lessThan99 > year) {
-                this.insertInfoMsg(e.target, '100세 이하만 가입 가능합니다.', 'red')
-
-            } else if (moreThan15 < year) {
-                this.insertInfoMsg(e.target, '만 14세 이상만 가입 가능합니다.', 'red')
-
-            } else {
-                this.insertInfoMsg(e.target, '', 'green')
-            }
-        })
-    },
-
-    registerMonthDayCheckEvent() {
-        const birthMonthInputField = document.querySelector('#birth-field select[name=month]')
-        const birthDayInputField   = document.querySelector('#birth-field input[name=day]')
-
-        birthMonthInputField.addEventListener('focusout', (e) => {
-            if (e.target.options[e.target.selectedIndex].value !== '월') {
-                birthDayInputField.focus()
-            }
-        })
-    },
-
-    registerBirthDayCheckEvent() {
-        const birthDayInputField = document.querySelector('#birth-field input[name=day]')
-        const birthMonthSelect   = document.querySelector('#birth-field select[name=month]')
-
-        birthDayInputField.addEventListener('focusout', (e) => {
-            let day = e.target.value
-            if (day === '') return
-            
-            const month = birthMonthSelect.options[birthMonthSelect.selectedIndex].value
-            day = Number(day)
-
-            if (month === '월') {
-                this.insertInfoMsg(e.target, '태어난 월을 먼저 선택하세요.', 'red')
-
-            } else if (isNaN(day) || day < 0 || this.getLastDay(month) < day) {
-                this.insertInfoMsg(e.target, '태어난 날짜를 다시 확인하세요.', 'red')
-
-            } else {
-                this.insertInfoMsg(e.target, '', 'green')
-            }
-        })
-    },
-
-    registerEmailCheckEvent() {
-        const emailInputField = document.querySelector('#email-field input[name=email]')
-
-        emailInputField.addEventListener('focusout', (e) => {
-            let text = e.target.value
-            if (text === '') return
-
-            text = text.toLowerCase()
-
-            if (!this.checkInputText(text, /^[a-z]+[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/)) {
-                this.insertInfoMsg(e.target, '이메일 주소를 다시 확인해주세요.', 'red')
-            } else {
-                this.insertInfoMsg(e.target, '', 'green')
-            }
-        })
-    },
-
-    registerPhoneCheck() {
-        const phoneInputField = document.querySelector('#phone-field input[name=phone]')
-
-        phoneInputField.addEventListener('focusout', (e) => {
-            const text = e.target.value
-            if (text === '') return
-
-            if (!this.checkInputText(text, /^(010)[0-9]{7,8}$/)) {
-                this.insertInfoMsg(e.target, '형식에 맞지 않는 번호입니다.', 'red')
-            } else {
-                this.insertInfoMsg(e.target, '', 'green')
-            }
-        })
+        callback(checkList.find(e => e.condition === true))
     }
 }
