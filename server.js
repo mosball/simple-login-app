4fdcae9004d7851acbccd42402f3fa1e215a5fce
Mosball <rnjsrldnd123@naver.com>
2019-09-05 15:26:46 +0900

    ADD server listening code in server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require('./app')
+const server = app.listen(process.env.PORT || 3000, '0.0.0.0')
+
+server.on('error', onError)
+server.on('listening', onListening)
+
+const onError = (error) => {
+    console.log(`# 에러 발생
+    # 시간 : ${new Date()}
+    # 내용 : ${error}`)
+}
+
+const onListening = () => {
+    console.log(`3000 port server listening!! ${new Date()}`)
+}
